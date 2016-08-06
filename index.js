4de6968f47a14ba4547cf47f7a3c0190d48a2930
Adi Eyal <adi@code4sa.org>
2016-08-06 10:58:53 +0200

    Repackaged d3

---
+++ index.js
@@ -0,0 +1,21 @@
+export {
+  range
+} from "d3-array";
+
+export {
+  json
+} from "d3-request";
+
+export {
+  scaleQuantize,
+} from "d3-scale";
+
+export {
+  select,
+  selectAll
+} from "d3-selection";
+
+export {
+  geoMercator,
+  geoPath,
+} from "d3-geo";
